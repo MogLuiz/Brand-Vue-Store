fb219dd0b3415e7f72660a8ab50102f31a305487
Luiz Henrique <luizhjramos@outlook.com>
2022-04-23 19:16:18 -0300

    test: running mirageJS API before the tests

--- components/ProductCart/test.unit.spec.js
+++ components/ProductCart/test.unit.spec.js
@@ -4,11 +4,24 @@ import { mount } from '@vue/test-utils'
 // Components
 import ProductCart from '../ProductCart'
 
+// Services
+import { makeServer } from '@/miragejs/server'
+
 describe('ProductCart - Unit', () => {
+    let server
+
+    beforeEach(() => {
+        server = makeServer({ environment: 'test' })
+    })
+
+    afterEach(() => {
+        server.shutdown()
+    })
+
     it('should mount the component', () => {
         const wrapper = mount(ProductCart, {
             propsData: {
-                product: {},
+                product: server.create('product'),
             },
         })
 
