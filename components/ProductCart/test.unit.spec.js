7996049da8f27f61c0c9e00b146d85d09eb01203
Luiz Henrique <luizhjramos@outlook.com>
2022-04-24 00:23:14 -0300

    test: event emitted assertion

--- components/ProductCart/test.unit.spec.js
+++ components/ProductCart/test.unit.spec.js
@@ -8,15 +8,20 @@ import ProductCart from '../ProductCart'
 import { makeServer } from '@/miragejs/server'
 
 const mountProductCart = () => {
-    return mount(ProductCart, {
-        propsData: {
-            product: server.create('product', {
-                title: 'Relógio bonito',
-                price: '25,90',
-                image: 'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-            }),
-        },
+    const product = server.create('product', {
+        title: 'Relógio bonito',
+        price: '25,90',
+        image: 'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
     })
+
+    return {
+        wrapper: mount(ProductCart, {
+            propsData: {
+                product,
+            },
+        }),
+        product,
+    }
 }
 
 describe('ProductCart - Unit', () => {
@@ -31,13 +36,13 @@ describe('ProductCart - Unit', () => {
     })
 
     it('should match snapshot', () => {
-        const wrapper = mountProductCart()
+        const { wrapper } = mountProductCart()
 
         expect(wrapper.element).toMatchSnapshot()
     })
 
     it('should mount the component', () => {
-        const wrapper = mountProductCart()
+        const { wrapper } = mountProductCart()
 
         expect(wrapper.vm).toBeDefined()
         expect(wrapper.text()).toContain('Relógio bonito')
@@ -45,11 +50,12 @@ describe('ProductCart - Unit', () => {
     })
 
     it('should emit the event addToCart with product object when button gets clicked', async () => {
-        const wrapper = mountProductCart()
+        const { wrapper, product } = mountProductCart()
 
         await wrapper.find('button').trigger('click')
 
         expect(wrapper.emitted().addToCart).toBeTruthy()
         expect(wrapper.emitted().addToCart.length).toBe(1)
+        expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
     })
 })
