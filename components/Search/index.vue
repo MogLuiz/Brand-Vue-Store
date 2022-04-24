d56012f4623df594dbb832a4994925df1709922b
Luiz Henrique <luizhjramos@outlook.com>
2022-04-24 17:32:17 -0300

    feat: implement watch term method

--- components/Search/index.vue
+++ components/Search/index.vue
@@ -29,6 +29,11 @@ export default {
             term: '',
         }
     },
+    watch: {
+        term() {
+            if (this.term === '') this.doSearch()
+        },
+    },
     methods: {
         doSearch() {
             this.$emit('doSearch', { term: this.term })
