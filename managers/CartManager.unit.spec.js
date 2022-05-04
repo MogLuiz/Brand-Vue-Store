77ef7b22930af6219c04f23b7d41fc947046c04c
Luiz Henrique <luizhjramos@outlook.com>
2022-05-04 20:14:56 -0300

    test(CartManager): mount the mirage test helper server

--- managers/CartManager.unit.spec.js
+++ managers/CartManager.unit.spec.js
@@ -1,7 +1,20 @@
 // CartManager
 import { CartManager } from '@/managers/CartManager'
 
+// Services
+import { makeServer } from '@/miragejs/server'
+
 describe('CartManager', () => {
+    let server
+
+    beforeEach(() => {
+        server = makeServer({ environment: 'test' })
+    })
+
+    afterEach(() => {
+        server.shutdown()
+    })
+
     it('should set cart to open', () => {
         const manager = new CartManager()
         const state = manager.open()
