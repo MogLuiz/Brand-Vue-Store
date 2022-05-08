76c50d57257da988b06ace71bd65527e43f2ab66
Luiz Henrique <luizhjramos@outlook.com>
2022-05-08 17:49:53 -0300

    test(ProductList): should display the total quantity of products

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -142,4 +142,11 @@ describe('ProductList - integration', () => {
         expect(wrapper.vm.searchTerm).toEqual('')
         expect(cards).toHaveLength(11)
     })
+
+    it('should display the total quantity of products', async () => {
+        const { wrapper } = await mountProductList(27)
+        const label = wrapper.find('[data-testid="total-quantity-label"]')
+
+        expect(label.text()).toEqual('27 Products')
+    })
 })
